6eaee09338f43dd55f7b7f05f033ef10c3da312a
alenab <k240207@mvrht.com>
2016-12-22 02:33:35 +0300

    Packages for models, fixture and tests created. Application manager created Helpers for session, contacts, groups created

--- python_training/test/test_create_contact.py
+++ python_training/test/test_create_contact.py
@@ -1,7 +1,7 @@
-from selenium.webdriver.firefox.webdriver import WebDriver
-from contact import Contact
 import pytest
-from application import Application
+
+from fixture.application import Application
+from model.contact import Contact
 
 
 @pytest.fixture
@@ -12,9 +12,9 @@ def app(request):
 
 
 def test_add_contact(app):
-        app.login(username="admin", password="secret")
+        app.session.login(username="admin", password="secret")
 
-        app.create_contact(
+        app.contact.create(
                         Contact(first_name="asd", middle_name="sdfgh", last_name="hjk", nickname="hoho", title="title",
                                 company="comp", address="add", home_tel="+12", mobile_tel="+12", work_tel="+12",
                                 fax="+12",
@@ -22,6 +22,6 @@ def test_add_contact(app):
                                 birth_month="January", birth_year="2001", anniversary_day="3", anniversary_month="May",
                                 anniversary_year="1999", address2="add",
                                 phone2="phone2", notes="jl"))
-        app.logout()
+        app.session.logout()
 
 
