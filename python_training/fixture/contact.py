dd1b667589313abd0cbee45dbd54731c94def967
alenab <k240207@mvrht.com>
2016-12-24 02:59:56 +0300

    Added tests for editing and deleting groups and contacts Test fixtures updated(new methods added)

--- python_training/fixture/contact.py
+++ python_training/fixture/contact.py
@@ -5,6 +5,8 @@ class ContactHelper:
 
     def create(self, contact):
         wd = self.app.wd
+        #go to home page
+        wd.find_element_by_link_text("home").click()
         # init contact creation
         wd.find_element_by_link_text("add new").click()
         # fill contact form
@@ -82,4 +84,101 @@ class ContactHelper:
         wd.find_element_by_name("notes").clear()
         wd.find_element_by_name("notes").send_keys(contact.notes)
         wd.find_element_by_xpath("//input[@type='submit' and @value='Enter']").click()
+
+    def delete_first_contact(self):
+        wd = self.app.wd
+        # go to home page
+        wd.find_element_by_link_text("home").click()
+        #check 1st contact
+        wd.find_element_by_name("selected[]").click()
+        # init contact deletion
+        wd.find_element_by_xpath("//input[@type='button' and @value='Delete']").click()
+        #submit deletion
+        wd.switch_to_alert().accept()
+
+    def edit_first(self, contact):
+        wd = self.app.wd
+        # go to home page
+        wd.find_element_by_link_text("home").click()
+        # init contact deletion
+        wd.find_element_by_xpath("//tr[@name='entry'][1]//img[@title='Edit']/..").click()
+        # fill contact form with new values
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(contact.first_name)
+        wd.find_element_by_name("middlename").click()
+        wd.find_element_by_name("middlename").clear()
+        wd.find_element_by_name("middlename").send_keys(contact.middle_name)
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(contact.last_name)
+        wd.find_element_by_name("nickname").click()
+        wd.find_element_by_name("nickname").clear()
+        wd.find_element_by_name("nickname").send_keys(contact.nickname)
+        wd.find_element_by_name("title").click()
+        wd.find_element_by_name("title").clear()
+        wd.find_element_by_name("title").send_keys(contact.title)
+        wd.find_element_by_name("company").click()
+        wd.find_element_by_name("company").clear()
+        wd.find_element_by_name("company").send_keys(contact.company)
+        wd.find_element_by_name("address").click()
+        wd.find_element_by_name("address").clear()
+        wd.find_element_by_name("address").send_keys(contact.address)
+        wd.find_element_by_name("home").click()
+        wd.find_element_by_name("home").clear()
+        wd.find_element_by_name("home").send_keys(contact.home_tel)
+        wd.find_element_by_name("mobile").click()
+        wd.find_element_by_name("mobile").clear()
+        wd.find_element_by_name("mobile").send_keys(contact.mobile_tel)
+        wd.find_element_by_name("work").click()
+        wd.find_element_by_name("work").clear()
+        wd.find_element_by_name("work").send_keys(contact.work_tel)
+        wd.find_element_by_name("fax").click()
+        wd.find_element_by_name("fax").clear()
+        wd.find_element_by_name("fax").send_keys(contact.fax)
+        wd.find_element_by_name("email").click()
+        wd.find_element_by_name("email").clear()
+        wd.find_element_by_name("email").send_keys(contact.email)
+        wd.find_element_by_name("email2").click()
+        wd.find_element_by_name("email2").clear()
+        wd.find_element_by_name("email2").send_keys(contact.email2)
+        wd.find_element_by_name("email3").click()
+        wd.find_element_by_name("email3").clear()
+        wd.find_element_by_name("email3").send_keys(contact.email3)
+        wd.find_element_by_name("homepage").click()
+        wd.find_element_by_name("homepage").clear()
+        wd.find_element_by_name("homepage").send_keys(contact.homepage)
+
+        if not wd.find_element_by_xpath(
+                                "//select[@name='bday']//option[@value='" + contact.birth_day + "']").is_selected():
+            wd.find_element_by_xpath("//select[@name='bday']//option[@value='" + contact.birth_day + "']").click()
+        if not wd.find_element_by_xpath(
+                                "//select[@name='bmonth']//option[@value='" + contact.birth_month + "']").is_selected():
+            wd.find_element_by_xpath("//select[@name='bmonth']//option[@value='" + contact.birth_month + "']").click()
+        wd.find_element_by_name("byear").click()
+        wd.find_element_by_name("byear").clear()
+        wd.find_element_by_name("byear").send_keys(contact.birth_year)
+
+        if not wd.find_element_by_xpath(
+                                "//select[@name='aday']//option[@value='" + contact.anniversary_day + "']").is_selected():
+            wd.find_element_by_xpath("//select[@name='aday']//option[@value='" + contact.anniversary_day + "']").click()
+        if not wd.find_element_by_xpath(
+                                "//select[@name='amonth']//option[@value='" + contact.anniversary_month + "']").is_selected():
+            wd.find_element_by_xpath(
+                "//select[@name='amonth']//option[@value='" + contact.anniversary_month + "']").click()
+        wd.find_element_by_name("ayear").click()
+        wd.find_element_by_name("ayear").clear()
+        wd.find_element_by_name("ayear").send_keys(contact.anniversary_year)
+        wd.find_element_by_name("address2").click()
+        wd.find_element_by_name("address2").clear()
+        wd.find_element_by_name("address2").send_keys(contact.address2)
+        wd.find_element_by_name("phone2").click()
+        wd.find_element_by_name("phone2").clear()
+        wd.find_element_by_name("phone2").send_keys(contact.phone2)
+        wd.find_element_by_name("notes").click()
+        wd.find_element_by_name("notes").clear()
+        wd.find_element_by_name("notes").send_keys(contact.notes)
+        #submit editing
+        wd.find_element_by_xpath("//input[@type='submit' and @value='Update']").click()
+
 
--- python_training/fixture/group.py
+++ python_training/fixture/group.py
@@ -13,6 +13,12 @@ class GroupHelper:
         # init group creation
         wd.find_element_by_name("new").click()
         # fill group form
+        self.fill_group_form(group, wd)
+        # submit group creation
+        wd.find_element_by_name("submit").click()
+        self.return_to_groups_page()
+
+    def fill_group_form(self, group, wd):
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
         wd.find_element_by_name("group_name").send_keys(group.name)
@@ -22,17 +28,26 @@ class GroupHelper:
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys(group.footer)
-        # submit group creation
-        wd.find_element_by_name("submit").click()
-        self.return_to_groups_page()
 
-    def delete_first_group(self):
+    def delete_first(self):
         wd = self.app.wd
         self.open_groups_page()
         #select first group
         wd.find_element_by_name("selected[]").click()
         #submit deletion
         wd.find_element_by_name("delete").click()
+        self.return_to_groups_page()
+
+    def edit_first(self, new_group):
+        wd = self.app.wd
+        self.open_groups_page()
+        #select first group
+        wd.find_element_by_name("selected[]").click()
+        #submit deletion
+        wd.find_element_by_name("edit").click()
+        self.fill_group_form(new_group, wd)
+        # submit deletion
+        wd.find_element_by_name("update").click()
         self.return_to_groups_page()
 
     def open_groups_page(self):
