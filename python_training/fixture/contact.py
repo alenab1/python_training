1204ff967d93d0d4910da04e5fbcbb88d00ab48f
alenab <k240207@mvrht.com>
2016-12-26 02:07:26 +0300

    Extracting more methods

--- python_training/fixture/contact.py
+++ python_training/fixture/contact.py
@@ -5,104 +5,16 @@ class ContactHelper:
 
     def create(self, contact):
         wd = self.app.wd
-        #go to home page
-        wd.find_element_by_link_text("home").click()
+        self.go_to_home_page()
         # init contact creation
         wd.find_element_by_link_text("add new").click()
         # fill contact form
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(contact.first_name)
-        wd.find_element_by_name("middlename").click()
-        wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(contact.middle_name)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(contact.last_name)
-        wd.find_element_by_name("nickname").click()
-        wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(contact.nickname)
-        wd.find_element_by_name("title").click()
-        wd.find_element_by_name("title").clear()
-        wd.find_element_by_name("title").send_keys(contact.title)
-        wd.find_element_by_name("company").click()
-        wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(contact.company)
-        wd.find_element_by_name("address").click()
-        wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(contact.address)
-        wd.find_element_by_name("home").click()
-        wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys(contact.home_tel)
-        wd.find_element_by_name("mobile").click()
-        wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(contact.mobile_tel)
-        wd.find_element_by_name("work").click()
-        wd.find_element_by_name("work").clear()
-        wd.find_element_by_name("work").send_keys(contact.work_tel)
-        wd.find_element_by_name("fax").click()
-        wd.find_element_by_name("fax").clear()
-        wd.find_element_by_name("fax").send_keys(contact.fax)
-        wd.find_element_by_name("email").click()
-        wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(contact.email)
-        wd.find_element_by_name("email2").click()
-        wd.find_element_by_name("email2").clear()
-        wd.find_element_by_name("email2").send_keys(contact.email2)
-        wd.find_element_by_name("email3").click()
-        wd.find_element_by_name("email3").clear()
-        wd.find_element_by_name("email3").send_keys(contact.email3)
-        wd.find_element_by_name("homepage").click()
-        wd.find_element_by_name("homepage").clear()
-        wd.find_element_by_name("homepage").send_keys(contact.homepage)
-
-        if not wd.find_element_by_xpath("//select[@name='bday']//option[@value='" + contact.birth_day + "']").is_selected():
-            wd.find_element_by_xpath("//select[@name='bday']//option[@value='" + contact.birth_day + "']").click()
-        if not wd.find_element_by_xpath(
-                                "//select[@name='bmonth']//option[@value='" + contact.birth_month + "']").is_selected():
-            wd.find_element_by_xpath("//select[@name='bmonth']//option[@value='" + contact.birth_month + "']").click()
-        wd.find_element_by_name("byear").click()
-        wd.find_element_by_name("byear").clear()
-        wd.find_element_by_name("byear").send_keys(contact.birth_year)
-
-        if not wd.find_element_by_xpath(
-                                "//select[@name='aday']//option[@value='" + contact.anniversary_day + "']").is_selected():
-            wd.find_element_by_xpath("//select[@name='aday']//option[@value='" + contact.anniversary_day + "']").click()
-        if not wd.find_element_by_xpath(
-                                "//select[@name='amonth']//option[@value='" + contact.anniversary_month + "']").is_selected():
-            wd.find_element_by_xpath("//select[@name='amonth']//option[@value='" + contact.anniversary_month + "']").click()
-        wd.find_element_by_name("ayear").click()
-        wd.find_element_by_name("ayear").clear()
-        wd.find_element_by_name("ayear").send_keys(contact.anniversary_year)
-        wd.find_element_by_name("address2").click()
-        wd.find_element_by_name("address2").clear()
-        wd.find_element_by_name("address2").send_keys(contact.address2)
-        wd.find_element_by_name("phone2").click()
-        wd.find_element_by_name("phone2").clear()
-        wd.find_element_by_name("phone2").send_keys(contact.phone2)
-        wd.find_element_by_name("notes").click()
-        wd.find_element_by_name("notes").clear()
-        wd.find_element_by_name("notes").send_keys(contact.notes)
+        self.fill_contact_form(contact)
+        #submit creation
         wd.find_element_by_xpath("//input[@type='submit' and @value='Enter']").click()
 
-    def delete_first_contact(self):
+    def fill_contact_form(self, contact):
         wd = self.app.wd
-        # go to home page
-        wd.find_element_by_link_text("home").click()
-        #check 1st contact
-        wd.find_element_by_name("selected[]").click()
-        # init contact deletion
-        wd.find_element_by_xpath("//input[@type='button' and @value='Delete']").click()
-        #submit deletion
-        wd.switch_to_alert().accept()
-
-    def edit_first(self, contact):
-        wd = self.app.wd
-        # go to home page
-        wd.find_element_by_link_text("home").click()
-        # init contact deletion
-        wd.find_element_by_xpath("//tr[@name='entry'][1]//img[@title='Edit']/..").click()
-        # fill contact form with new values
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
         wd.find_element_by_name("firstname").send_keys(contact.first_name)
@@ -148,7 +60,6 @@ class ContactHelper:
         wd.find_element_by_name("homepage").click()
         wd.find_element_by_name("homepage").clear()
         wd.find_element_by_name("homepage").send_keys(contact.homepage)
-
         if not wd.find_element_by_xpath(
                                 "//select[@name='bday']//option[@value='" + contact.birth_day + "']").is_selected():
             wd.find_element_by_xpath("//select[@name='bday']//option[@value='" + contact.birth_day + "']").click()
@@ -158,7 +69,6 @@ class ContactHelper:
         wd.find_element_by_name("byear").click()
         wd.find_element_by_name("byear").clear()
         wd.find_element_by_name("byear").send_keys(contact.birth_year)
-
         if not wd.find_element_by_xpath(
                                 "//select[@name='aday']//option[@value='" + contact.anniversary_day + "']").is_selected():
             wd.find_element_by_xpath("//select[@name='aday']//option[@value='" + contact.anniversary_day + "']").click()
@@ -178,6 +88,31 @@ class ContactHelper:
         wd.find_element_by_name("notes").click()
         wd.find_element_by_name("notes").clear()
         wd.find_element_by_name("notes").send_keys(contact.notes)
+
+    def delete_first_contact(self):
+        wd = self.app.wd
+        self.go_to_home_page()
+        self.click_first_entry_in_list()
+        # init contact deletion
+        wd.find_element_by_xpath("//input[@type='button' and @value='Delete']").click()
+        #submit deletion alert
+        wd.switch_to_alert().accept()
+
+    def click_first_entry_in_list(self):
+        wd = self.app.wd
+        wd.find_element_by_name("selected[]").click()
+
+    def go_to_home_page(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("home").click()
+
+    def edit_first(self, contact):
+        wd = self.app.wd
+        self.go_to_home_page()
+        # init contact editing
+        wd.find_element_by_xpath("//tr[@name='entry'][1]//img[@title='Edit']/..").click()
+        # fill contact form with new values
+        self.fill_contact_form(contact)
         #submit editing
         wd.find_element_by_xpath("//input[@type='submit' and @value='Update']").click()
 
--- python_training/fixture/group.py
+++ python_training/fixture/group.py
@@ -11,12 +11,17 @@ class GroupHelper:
         wd = self.app.wd
         self.open_groups_page()
         # init group creation
-        wd.find_element_by_name("new").click()
+        self.init_group_creation(wd)
         # fill group form
         self.fill_group_form(group, wd)
-        # submit group creation
-        wd.find_element_by_name("submit").click()
+        self.submit_group_creation(wd)
         self.return_to_groups_page()
+
+    def submit_group_creation(self, wd):
+        wd.find_element_by_name("submit").click()
+
+    def init_group_creation(self, wd):
+        wd.find_element_by_name("new").click()
 
     def fill_group_form(self, group, wd):
         wd.find_element_by_name("group_name").click()
@@ -32,8 +37,7 @@ class GroupHelper:
     def delete_first(self):
         wd = self.app.wd
         self.open_groups_page()
-        #select first group
-        wd.find_element_by_name("selected[]").click()
+        self.click_first_entry_in_list()
         #submit deletion
         wd.find_element_by_name("delete").click()
         self.return_to_groups_page()
@@ -41,14 +45,19 @@ class GroupHelper:
     def edit_first(self, new_group):
         wd = self.app.wd
         self.open_groups_page()
-        #select first group
-        wd.find_element_by_name("selected[]").click()
-        #submit deletion
-        wd.find_element_by_name("edit").click()
+        self.click_first_entry_in_list()
+        self.open_modification_form()
         self.fill_group_form(new_group, wd)
-        # submit deletion
         wd.find_element_by_name("update").click()
         self.return_to_groups_page()
+
+    def open_modification_form(self):
+        wd = self.app.wd
+        wd.find_element_by_name("edit").click()
+
+    def click_first_entry_in_list(self):
+        wd = self.app.wd
+        wd.find_element_by_name("selected[]").click()
 
     def open_groups_page(self):
         wd = self.app.wd
